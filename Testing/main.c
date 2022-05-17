622147b5096dc92b67b5637b48336113fdc91725
simonsanmueldk <simonsamuelhol@gmail.com>
2022-05-17 08:48:00 +0200

    FFF added

---
+++ Testing/main.c
@@ -0,0 +1,18 @@
+/*
+ * Testing.c
+ *
+ * Created: 17/05/2022 08.46.32
+ * Author : simon
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
