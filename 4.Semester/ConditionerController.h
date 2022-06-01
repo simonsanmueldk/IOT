04acd93d5e4ddb820133fbc649ec6ede45a5c8b7
Radovan Santa <84182964+radovansanta@users.noreply.github.com>
2022-06-01 23:06:15 +0200

    Added comments

--- 4.Semester/ConditionerController.h
+++ 4.Semester/ConditionerController.h
@@ -6,10 +6,14 @@
  *  Author: Dorin Pascal
  */ 
 
+//---HEADER---
+
+// Includes
 #include "Configuration.h"
 #include "TempHumSensor.h"
 #include <rc_servo.h>
 
+// Methods
 void conditioner_controller_create(UBaseType_t priority);
 void Conditioner_Task_Run(void* pvParameters);
 void Conditioner_task();
