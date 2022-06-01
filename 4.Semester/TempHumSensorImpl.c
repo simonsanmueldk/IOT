8243cb58535f62a46c971e99d6c8f5973d36d99e
Radovan Santa <84182964+radovansanta@users.noreply.github.com>
2022-06-01 21:09:03 +0200

    Added comments

--- 4.Semester/TempHumSensorImpl.c
+++ 4.Semester/TempHumSensorImpl.c
@@ -14,28 +14,30 @@
 #include "TempHumSensor.h"
 #include "event_groups.h"
 
-/* Event Groups */
+// Event Groups
 extern EventGroupHandle_t _meassureEventGroup ;
 extern EventGroupHandle_t _dataReadyEventGroup;
- //Bit for set
- #define TEMPERATURE_HUMIDITY_READY_BIT (1 << 1)
 
+//Bit to set event groups
+#define TEMPERATURE_HUMIDITY_READY_BIT (1 << 1)
 
-/* Tick type */
+//Tick types
 TickType_t xLastWakeTime;
 TickType_t xFrequency;
 
+//Default temperature and humidity values
 float temperature = 0;
 uint16_t humidity = 0;
+
 EventBits_t event_measure;
 
+//Structure
 typedef struct Temp_Humidity_Sensor {
 	uint16_t temperature_data;
 	float humidity_data;
 }Temp_Humidity_Sensor;
 
-//-------Constructor-------------------
-
+//Constructor using pvPortMalloc()
 void Temp_Humidty_sensor_create()
 {
 	temperature_humdity_sensor_t new_temperature_humidty = pvPortMalloc(sizeof(Temp_Humidity_Sensor));
@@ -43,37 +45,37 @@ void Temp_Humidty_sensor_create()
 	{
 		return NULL;
 	}
+	//Set a default values for a created structure
 	new_temperature_humidty->humidity_data=humidity;
 	new_temperature_humidty->temperature_data=temperature;
 	
 }
-//------Get temperature data-----------
-
+// Get temperature data
 uint16_t get_temperature_data()
 {
 	return (uint16_t)temperature;
 }
 
-//-------Get humidity data--------
+// Get humidity data
 uint16_t get_humidity_data()
 {
 	return humidity;
 }
 
+
+//Method to create a task to measure the temperature and humidity
 void tempHum_taskCreate(UBaseType_t task_priority){
-	
 	xTaskCreate(
-	tempHum_Task
-	,  "Temperature_Humidity"  // A name just for humans
-	,  configMINIMAL_STACK_SIZE  // This stack size can be checked & adjusted by reading the Stack Highwater
+	tempHum_Task //Task
+	,  "Temperature_Humidity"  // A name of task
+	,  configMINIMAL_STACK_SIZE  // This stack size
 	,  NULL
-	,  task_priority // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+	,  task_priority // Priority od the task
 	,  NULL );
-	
 }
 
 
-/* Task to run for the sensors to work */
+// Task to run for the temperature/humididty sensors to work
 void tempHum_Run() {
 	event_measure = xEventGroupWaitBits(
 	_meassureEventGroup,
@@ -87,20 +89,27 @@ void tempHum_Run() {
 		vTaskDelay( pdMS_TO_TICKS(100UL));
 		if (HIH8120_OK != hih8120_wakeup())
 		{
+			//In case the sensor is not ready yet then delay method
 			vTaskDelay(pdMS_TO_TICKS(100UL));
+			//Print error message for a troubleshhoting purpose
 			printf("\n ---Temp/humidity sensor couldn't wake up trying again--");
 			while(HIH8120_OK == hih8120_wakeup())
 			{
 				vTaskDelay(pdMS_TO_TICKS(50UL));
 			}
 		}
+		//Perform measurement of temperature and humidity 
 		hih8120_measure();
+		//Delay method
 		vTaskDelay(pdMS_TO_TICKS(50UL));
 		if (HIH8120_OK == hih8120_measure() )
 		{
+			//Delay method
 			vTaskDelay(pdMS_TO_TICKS(100UL));
+			//Get the measured temperature and humidity
 			humidity =  hih8120_getHumidityPercent_x10();
 			temperature = hih8120_getTemperature_x10();
+			//Set the bits of the event group
 			xEventGroupSetBits(_dataReadyEventGroup, TEMPERATURE_HUMIDITY_READY_BIT);
 		}
 	}
@@ -109,8 +118,10 @@ void tempHum_Run() {
 void tempHum_Task( void *pvParameters )
 {
 	(void)pvParameters;
+	//Infinite loop that runs the task
 	for(;;)
 	{
+		//Calling method to performs measurements
 		tempHum_Run();
 	}
 }
