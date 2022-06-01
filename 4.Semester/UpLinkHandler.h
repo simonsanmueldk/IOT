6f0618de14cbad4a83c1b3105c9617e94573d24c
Radovan Santa <84182964+radovansanta@users.noreply.github.com>
2022-06-01 22:21:50 +0200

    Added comments

--- 4.Semester/UpLinkHandler.h
+++ 4.Semester/UpLinkHandler.h
@@ -1,10 +1,13 @@
 #pragma once
 
+//---HEADER---
+
+// Includes
 #include "ATMEGA_FreeRTOS.h"
 #include "portmacro.h"
 #include "message_buffer.h"
 
-
+// Methods
 void upLink_create(UBaseType_t priority);
 void lora_handler_task();
 void upLinkHandler_StartTask();
