f34c6fbe365317bb5196814e192e93f28cbcc784
Radovan Santa <84182964+radovansanta@users.noreply.github.com>
2022-06-01 23:13:25 +0200

    Added comments

--- 4.Semester/Application.h
+++ 4.Semester/Application.h
@@ -6,8 +6,11 @@
  *  Author: Dorin Pascal
  */ 
 
+//---HEADER---
+
 #pragma once
 
+// Methods
 void Application_Task(void* pvParameters);
 void application_task_create(UBaseType_t task_priority);
 void application_run(TickType_t* xLastWakeTime,TickType_t xFrequency);
