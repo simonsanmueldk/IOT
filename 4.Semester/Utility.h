a4043d1b2d022a3e750c53b28ffcff5b31866142
Radovan Santa <84182964+radovansanta@users.noreply.github.com>
2022-06-01 22:13:57 +0200

    Added Comments

--- 4.Semester/Utility.h
+++ 4.Semester/Utility.h
@@ -5,22 +5,29 @@
  * Created: 5/24/2022 7:45:09 PM
  *  Author: Dorin Pascal
  */ 
+
+//---HEADER---
+
+// Includes
 #include "message_buffer.h"
 #include "event_groups.h"
 #include <task.h>
 #include "semphr.h"
 
-MessageBufferHandle_t xMessageBuffer;
-MessageBufferHandle_t downlinkMessageBuffer;
-SemaphoreHandle_t mutex;
+MessageBufferHandle_t xMessageBuffer; // Message Buffer
+MessageBufferHandle_t downlinkMessageBuffer; // Downlink Mesage Buffer
+SemaphoreHandle_t mutex; // Mutex
 
+// Defines
 #define CO2_BIT (1<<1)
 #define TEMPERATURE_HUMIDITY_BIT (1 << 0)
 
-
+// Event groups
 EventGroupHandle_t _meassureEventGroup ;
 EventGroupHandle_t _dataReadyEventGroup;
 
+
+//Methods
 void create_message_buffer();
 void create_event_groups();
 void create_mutex();
