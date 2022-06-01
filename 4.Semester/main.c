f95f6b5988fe81b4a123c78180e4d2d6b74b91d8
Radovan Santa <84182964+radovansanta@users.noreply.github.com>
2022-06-01 21:54:46 +0200

    Added comments

--- 4.Semester/main.c
+++ 4.Semester/main.c
@@ -34,34 +34,37 @@
 #include "Utility.h"
 #include "Configuration.h"
 
+// Create a message buffer
 extern MessageBufferHandle_t downlinkMessageBuffer;
 
+// Method that creates needed tasks with its prioority
 void create_tasks()
 {
-	
 	application_task_create(3);
 	co2_task_create(1);
 	tempHum_taskCreate(1);
-	//ToDo maybe to move inside the application task
 	SensorDataPackage_create();
 	conditioner_controller_create(4);
 }
 
+
+// Method that initialise sensors and servo
 void drivers_initialisation()
-{
-	
-	  hih8120_initialise();
-	  mh_z19_initialise(ser_USART3);
-	  rc_servo_initialise();
+{	
+	  hih8120_initialise(); // Initialise Temperature/Humidity sensor
+	  mh_z19_initialise(ser_USART3); // Initialise CO2 sensor
+	  rc_servo_initialise(); // Initialise RC Servo
 }
 
+// Method that create Utility
 void createUtility()
 {
-	create_event_groups();
-	create_message_buffer();
-	create_mutex();
+	create_event_groups(); // Create event groups
+	create_message_buffer(); // Create message buffer
+	create_mutex(); // Create mutex
 }
 
+// Method that initialise the system
 void initialiseSystem()
 {
 	// Set output ports for leds used in the example
@@ -76,18 +79,15 @@ void initialiseSystem()
 	lora_driver_initialise(1, downlinkMessageBuffer);
 	// Create UpLinkTask and start it up with priority 4 and setup the LoRaWan
 	upLink_create(4);
-	
 	// Create DownlinkTask and start it up with priority 5
 	lora_DownLinkHandler_create(5);
-
-    
 }
 
 /*-----------------------------------------------------------*/
 int main(void)
 {
-	initialiseSystem(); // Must be done as the very first thing!!
-	printf("Program started \n");
+	initialiseSystem(); // Initialise system must be done as the very first thing
+	printf("Program started \n"); // Print just for troubleshooting purpose
 	vTaskStartScheduler(); // Initialise and run the freeRTOS scheduler. Execution should never return from here.
 	
 }
