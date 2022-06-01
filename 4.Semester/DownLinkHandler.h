3de6c003a733dc53c257c1a5e222e25e49f9cd0b
Radovan Santa <84182964+radovansanta@users.noreply.github.com>
2022-06-01 23:03:26 +0200

    Added comments

--- 4.Semester/DownLinkHandler.h
+++ 4.Semester/DownLinkHandler.h
@@ -1,8 +1,12 @@
+//---HEADER---
+
 #pragma once
 
+// Includes
 #include "ATMEGA_FreeRTOS.h"
 #include "portmacro.h"
 
+//Methods
 void lora_DownLinkHandler_create(UBaseType_t priority);
 void lora_DownLinkHandler_task();
 void lora_DownLinkHandler_startTask();
