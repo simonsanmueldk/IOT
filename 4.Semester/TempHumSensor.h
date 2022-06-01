9e3d42f7f305a99b1911f8a0e69254d2104cb03d
Radovan Santa <84182964+radovansanta@users.noreply.github.com>
2022-06-01 22:54:47 +0200

    Added comments

--- 4.Semester/TempHumSensor.h
+++ 4.Semester/TempHumSensor.h
@@ -4,17 +4,23 @@
  * Created: 01/05/2022 21.39.51
  *  Author: Simon
  */ 
+
+//---HEADER---
+
 #pragma once
 
+// Includes
 #include "event_groups.h"
 #include <stdint.h>
 #include <stdio.h>
 
+// Structure
+typedef struct Temp_Humidity_Sensor* temperature_humdity_sensor_t;
 
-
+// Methods
 void tempHum_taskCreate(UBaseType_t task_priority);
 void tempHum_Task( void *pvParameters );
 void tempHum_Run();
-typedef struct Temp_Humidity_Sensor* temperature_humdity_sensor_t;
-
-void Temp_Humidty_sensor_create();uint16_t get_temperature_data();uint16_t get_humidity_data();
+void Temp_Humidty_sensor_create();
+uint16_t get_temperature_data();
+uint16_t get_humidity_data();
