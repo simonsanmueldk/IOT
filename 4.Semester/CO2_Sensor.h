3605df0857f84cdce942a1456beb017a0641ca84
Radovan Santa <84182964+radovansanta@users.noreply.github.com>
2022-06-01 23:27:11 +0200

    Added comments

--- 4.Semester/CO2_Sensor.h
+++ 4.Semester/CO2_Sensor.h
@@ -4,8 +4,11 @@
  * Created: 05/05/2022 12:35:45
  *  Author: Chen
  */ 
+
+//---HEADER---
 #pragma once
 
+// Includes
 #include "ATMEGA_FreeRTOS.h"
 #include <stdint.h>
 #include <stdio.h>
@@ -15,8 +18,10 @@
 #include <avr/io.h>
 #include "event_groups.h"
 
+// Structure
 typedef struct CO2_Sensor* co2_sensor_t;
 
+// Methods
 void CO2_sensor_create();
 void myCo2CallBack(uint16_t *ppm);
 void co2_task_create(UBaseType_t task_priority);
